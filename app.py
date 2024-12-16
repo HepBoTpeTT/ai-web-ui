e4987615788ba68235043a1afe7d85cfa82b9a96
Denis <118465932+HepBoTpeTT@users.noreply.github.com>
2024-12-16 22:28:04 +0300

    ya yego vsse e

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import soundfile as sf
 import webbrowser
 from pydub import AudioSegment
 import os
+from json import loads
 
 app = Flask(__name__)
 app.config['SERVER_NAME'] = 'localhost:5000'
@@ -40,7 +41,7 @@ def fn_clearvoice_ss(input_wav, folder_name):
 
 ## Audio enhancement func
 @spaces.GPU
-def fn_clearvoice_se(input_wav, sr):
+def fn_clearvoice_se(input_wav, sr, sep_flag, file_name = None):
     if sr == "16000 Hz":
         myClearVoice = ClearVoice(task='speech_enhancement', model_names=['FRCRN_SE_16K'])
         fs = 16000
@@ -53,12 +54,17 @@ def fn_clearvoice_se(input_wav, sr):
         output_wav = output_wav_dict[key]
     else:
         output_wav = output_wav_dict
-    sf.write(input_wav, output_wav, fs)
-    return input_wav
+
+    if sep_flag: writing_path = input_wav
+    else: writing_path = str(path / audio_processed_path / file_name[:-4] / 'operator.wav')
+
+    sf.write(writing_path, output_wav, fs)
+    return writing_path
 ##
 
-def generate_response(files):
+def generate_response(files, seps):
     with app.app_context():
+        result_list = []
         for file in files:
             print('----------------------')
             file_path = str(path / 'audio-source' / f'{file.filename[:-4]}_16k.wav')
@@ -66,20 +72,17 @@ def generate_response(files):
 
             new_folder_path = path / audio_processed_path / file.filename[:-4]
             new_folder_path.mkdir(exist_ok=True)
-
             link_path = str(audio_processed_path / file.filename[:-4] / 'operator.wav').replace('\\', '/')
 
-            print(
-                '',
-                f'Сохранённый файл: {new_folder_path / "operator.wav"}',
-                f'Путь для ссылки {link_path}',
-                '',
-                sep="\n"
-            )
 
-            operator = fn_clearvoice_ss(file_path, new_folder_path)
-            operator_denoised = fn_clearvoice_se(operator, "16000 Hz")
-            emotion = predict_emotion(operator_denoised)
+            if seps[file.filename]:
+                operator = fn_clearvoice_ss(file_path, new_folder_path)
+                operator_denoised = fn_clearvoice_se(input_wav=operator, sr="16000 Hz", sep_flag=seps[file.filename])
+                emotion = predict_emotion(operator_denoised)
+                
+            else:
+                file_denoised = fn_clearvoice_se(file_path, "16000 Hz", seps[file.filename], file.filename)
+                emotion = predict_emotion(file_denoised)
 
             os.remove(file_path)
 
@@ -88,8 +91,10 @@ def generate_response(files):
             audio_path = url_for('static', filename=link_path)
             image = url_for('static', filename='images/default-audio-image.webp')
 
-            yield render_template('audio-card.html', name = f'{file.filename} / operator', image = image,
-                                audio_path = audio_path, emotion = emotion)
+            result_list.append(render_template('audio-card.html', name = f'{file.filename} / operator', image = image,
+                                audio_path = audio_path, emotion = emotion))
+            
+        return result_list
 
 def save_file_in_good_format(file):
     try:
@@ -117,6 +122,9 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     files = request.files.getlist('files')
+    separation_flags = loads(request.form.get('sepsFlags'))
+
+    print(separation_flags, type(separation_flags))
 
     # Предварительное сохранение файлов
     tuple(map(lambda file: file.save(Path('temp', file.filename)), files))
@@ -124,7 +132,7 @@ def upload():
     # Пересохранение файлов в нужный формат
     tuple(map(lambda item: save_file_in_good_format(item), files))
 
-    return jsonify({'audios' : list(map(lambda i: i, generate_response(files)))})
+    return jsonify({'audios' : generate_response(files, separation_flags)})
 
 @app.route('/update_single', methods=['POST'])
 def update_single():
